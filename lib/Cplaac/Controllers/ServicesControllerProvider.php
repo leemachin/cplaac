d394c72d0b57b60c1cd08615f586b67156e3997d
Lee Machin <ljmachin@gmail.com>
2015-02-17 23:10:36 +0000

    Use the form builder to generate this thing

--- lib/Cplaac/Controllers/ServicesControllerProvider.php
+++ lib/Cplaac/Controllers/ServicesControllerProvider.php
@@ -2,8 +2,13 @@
 
 namespace Cplaac\Controllers;
 
+use Cplaac\Services\Service;
+
 use Silex\Application;
 use Silex\ControllerProviderInterface;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\Request;
 
 class ServicesControllerProvider implements ControllerProviderInterface {
   
@@ -15,13 +20,72 @@ class ServicesControllerProvider implements ControllerProviderInterface {
     });
 
     $services->get('/new', function() use ($app) {
-      return $app['twig']->render('services/new.twig');
+      $form = $this->createForm($app);
+
+      return $app['twig']->render('services/new.twig', [
+        'form' => $form->createView()
+      ]);
+
     })->before($app['requireAuth']);
 
-    $services->post('/create', function() use ($app) {
-  
+    $services->post('/create', function(Request $request) use ($app) {
+      $form = $this->createForm($app);
+      $form->handleRequest($request);
+
+      if ($form->isValid()) {
+      } else {
+      }
     })->before($app['requireAuth']);
 
     return $services;
+  }
+
+  private function createForm($app, $data = null) {
+    $serviceForm = $app['form.factory']->createNamedBuilder('service', 'form', $data);
+    $infoForm = $app['form.factory']->createNamedBuilder('additional_info', 'form', $data, [
+      'label' => false,
+      'required' => false
+    ]);
+
+    $serviceForm
+      ->add('service_name', 'text', ['constraints' => new Assert\NotBlank])
+      ->add('address', 'textarea', ['constraints' => new Assert\NotBlank])
+      ->add('contact_name', 'text', ['constraints' => new Assert\NotBlank])
+      ->add('contact_phone', 'text', ['constraints' => new Assert\NotBlank])
+      ->add('contact_email', 'email', ['constraints' => new Assert\Email])
+      ->add($infoForm);
+     
+     
+    $infoForm ->add('staffing', 'textarea')
+      ->add('type', 'textarea')
+      ->add('camh', 'textarea')
+      ->add('referral_pathway', 'textarea')
+      ->add('referral_rules', 'textarea');
+
+    $infoForm->add('client_groups', 'choice', [
+      'choices' => Service::CLIENT_GROUPS,
+      'expanded' => true,
+      'multiple' => true,
+      'constraints' => new Assert\Choice(range(0, count(Service::CLIENT_GROUPS)))
+    ]);
+
+    $infoForm->add('client_groups_other', 'text');
+
+    $infoForm
+      ->add('screening', 'textarea')
+      ->add('psychometrics', 'textarea')
+      ->add('court_proceedings', 'textarea')
+      ->add('therapy_models', 'textarea');
+
+    $infoForm->add('services_offered', 'choice', [
+      'choices' => Service::SERVICES_OFFERED,
+      'expanded' => true,
+      'multiple' => true,
+      'constraints' => new Assert\Choice(range(0, count(Service::SERVICES_OFFERED)))
+    ]);
+
+    $infoForm->add('services_offered_other', 'textarea');
+
+    return $serviceForm->getForm();
   }
 }
--- lib/Cplaac/Services/Service.php
+++ lib/Cplaac/Services/Service.php
@@ -2,6 +2,55 @@
 
 namespace Cplaac\Services;
 
-class Service extends Model {
+use Cplaac\Core\Entity;
+
+class Service extends Entity {
+
+  protected $service_name,
+            $address,
+            $contact_name,
+            $contact_phone,
+            $contact_email,
+            $additional_info;
+
+  protected $table_name = "cplaac_services";
+
+  const CLIENT_GROUPS = [
+    'Children in residential homes',
+    'Children in long term foster placements',
+    'Children in short term foster placements',
+    'Children en route to adoptions',
+    'Potential adopters',
+    'Children who are adopted',
+    'Children who are accommodated (no care order)',
+    'Children who live in kinship placement or special guardianship',
+    'Children who are at home on a care order',
+    'Children who are on a child protection plan, in need, or considered to be at the edge of care',
+    'Birth parents who have had children removed (plan is to rehabilitate)',
+    'Birth parents who have had children removed (plan is to not rehabilitate)'
+  ];
+
+  const SERVICES_OFFERED = [
+    'Consultation for professionals',
+    'Consultation for carers',
+    'Attending professional/child protection meetings, or LAC reviews',
+    'Attending school meetings',
+    'Input into panels',
+    'Advice on matching or placement choice',
+    'Training for staff',
+    'Training for carers',
+    'Research',
+    'Audit/service evaluation/clinical governance',
+    'Strategic/systemic involvement in organisation',
+    'Liaison between agencies'
+  ];
+
+  public function isValid() {
+
+  }
+
+  public function additional_info() {
+    return json_decode($this->additional_info);
+  }
 
 }
